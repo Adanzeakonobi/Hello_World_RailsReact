8d50fea80d89b755e989fbd2ce556d3570b16a41
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-09-12 14:15:45 -0600

    Create an API endpoint that selects a random greeting

---
+++ app/controllers/api/greetings_controller.rb
@@ -0,0 +1,7 @@
+class Api::GreetingsController < ApplicationController
+    def index
+        offset = rand(Message.count)
+        puts "Message: #{@random_record}"
+        @random_record = Message.offset(offset).first
+    end
+end
