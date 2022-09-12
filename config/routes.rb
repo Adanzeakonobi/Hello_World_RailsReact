2dbc84d65dfa18f289a07bd07ced2547c2ce2b8a
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-09-12 14:16:08 -0600

    Create a static view that will be the root of your app.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  root "static#index"
+
+  namespace :api, defaults: {format: :json} do
+    get '/greetings', to: 'greetings#index'
+  end
+
+  get '*page', to: "static#index", constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
 end
